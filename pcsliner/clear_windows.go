5a9db93f8392af5010ad4bbe9d410d2a73d93b2c
LinskRuis.3.6GHz <mgr9525@qq.com>
2019-09-19 15:12:44 +0800

    dev-

--- pcsliner/clear_windows.go
+++ pcsliner/clear_windows.go
@@ -5,8 +5,10 @@ import (
 	_ "unsafe" // for go:linkname
 )
 
-//go:linkname eraseScreen github.com/iikira/BaiduPCS-Go/vendor/github.com/peterh/liner.(*State).eraseScreen
-func eraseScreen(s *liner.State)
+////go:linkname eraseScreen github.com/iikira/BaiduPCS-Go/vendor/github.com/peterh/liner.(*State).eraseScreen
+func eraseScreen(s *liner.State) {
+
+}
 
 // ClearScreen 清空屏幕
 func (pl *PCSLiner) ClearScreen() {
